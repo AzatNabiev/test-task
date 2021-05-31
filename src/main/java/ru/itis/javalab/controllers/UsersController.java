38969d7d8813e41f3b0a16d8c458a753e475f246
AzatNabiev <55456436+AzatNabiev@users.noreply.github.com>
2021-06-01 00:12:07 +0300

    finished

--- src/main/java/ru/itis/javalab/controllers/UsersController.java
+++ src/main/java/ru/itis/javalab/controllers/UsersController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itis.javalab.dto.UserDto;
 import ru.itis.javalab.form.UserForm;
@@ -12,6 +13,7 @@ import ru.itis.javalab.services.UserService;
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
     private UserService userService;
 
@@ -20,7 +22,7 @@ public class UsersController {
         this.userService = userService;
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<UserDto> addUser(@Valid @RequestBody UserForm userForm) {
         return ResponseEntity.ok(userService.addUser(userForm));
     }
